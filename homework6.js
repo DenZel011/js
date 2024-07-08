0478594f677fbb29427887086158beb5c9bb0e3c
DenZel <1510306den@gmail.com>
2024-07-08 17:44:39 +0300

    Homework 6

---
+++ homework6.js
@@ -0,0 +1,72 @@
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+};
+
+
+function findValueByKey(Object, companyName)
+{
+	if(companyName == Object.name)
+	{
+		console.log(Object);
+		return Object;
+	}
+	else
+	{
+		let arr = Object.clients || Object.partners;
+		
+		for(let i in arr)
+		{
+			if(String(companyName).includes(arr[i].name))
+			{
+				Object = arr[i];
+			}
+			else
+			{
+				continue;
+			}
+		}
+
+		return findValueByKey(Object, companyName);		
+	}
+}
+
+findValueByKey(company, 'Клієнт 1.2.3');
