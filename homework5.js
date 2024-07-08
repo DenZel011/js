ba55635d335d46ff01cafed69713f88be7917055
DenZel011 <35483031+DenZel011@users.noreply.github.com>
2024-07-08 14:14:58 +0300

    Update homework5.js

--- homework5.js
+++ homework5.js
@@ -20,3 +20,92 @@ function checkProbabilityTheory(count)
 }
 
 checkProbabilityTheory(3)
+
+
+let student =
+{
+	weight : 115,
+	height : 190,
+	name : "Denys",
+	getInfo : function()
+	{
+		for(let key in this)
+		{
+			if(key != "getInfo")
+				console.log(key + " : " + this[key]);
+		}
+	}
+};
+
+student.getInfo();
+
+student.age = 26.
+
+student.getInfo();
+
+
+var services = 
+{
+	haircut : "60 грн",
+	shawing : "80 грн",
+	headwash : "100 грн",
+	price : function()
+	{
+		let sum = 0;
+		let curPrice = 0;
+		
+		for (let key in this)
+		{
+			if(typeof(this[key]) != 'function')
+			{
+				curPrice = parseFloat(this[key]);
+				sum += curPrice;
+			}
+		}
+	
+		console.log("Overall sum = " + sum);
+	},
+	minPrice : function()
+	{
+		let price = Infinity;
+
+		for (var key in this)
+		{
+			curPrice = parseFloat(this[key]);
+
+			if(curPrice <= price)
+			{
+				price = curPrice;
+			}
+		}
+	
+		console.log("Minimum service price = " + price);
+	},
+	maxPrice : function()
+	{
+		let price = 0;
+
+		for (let key in this)
+		{
+			let curPrice = parseFloat(this[key]);
+
+			if(curPrice > price)
+			{
+				price = curPrice;
+			}
+		}
+
+		console.log("Maximum service price = " + price);
+	}
+};
+
+services.price();
+services.minPrice();
+services.maxPrice();
+
+services.wallbreak = "20 грн";
+services.legbreak = "120 грн";
+
+services.price();
+services.minPrice();
+services.maxPrice();
